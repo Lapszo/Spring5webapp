21118c662ac9cdb16ddedda08d5ef050357cd9a3
Lapszo <lapszotomasz@gmail.com>
2019-04-28 13:03:11 +0200

    Initializing data with SpringF events 2

--- src/main/java/lapszo/firstspringapp/spring5app/repositories/AuthorRepository.java
+++ src/main/java/lapszo/firstspringapp/spring5app/repositories/AuthorRepository.java
@@ -3,5 +3,7 @@ package lapszo.firstspringapp.spring5app.repositories;
 import lapszo.firstspringapp.spring5app.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
